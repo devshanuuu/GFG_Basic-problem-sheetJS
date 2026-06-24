776b8d777bb47ec3af52b8ef40442c5dd21cf5b8
devshanuuu <shanuuu0207@gmail.com>
2026-06-24 13:59:26 +0530

    API file added

---
+++ FetchData_API.js
@@ -0,0 +1,15 @@
+async function getUsers() {
+    
+    try {
+      const response = await fetch("http://localhost:3000/api/users");
+
+      const data = await response.json;
+      console.log(data);
+      
+    } catch(err) {
+     console.log(err);
+     
+    }
+}
+
+getUsers();
